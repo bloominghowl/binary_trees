8b6096ab834b35e9a8ab72e9f31f8e1cdad4acad
Ayivi Angela <104152275+bloominghowl@users.noreply.github.com>
2023-05-30 15:10:01 +0000

    Update 0-binary_tree_node.c

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -3,21 +3,23 @@
 
 /**
  * binary_tree_node - creates a binary tree node
- * @parent: parnt of the node to create
- * @value: value to store in new node
+ * @parent: parent of the node to create
+ * @value: value to store in the new node
  *
- * Return: pointer to the new node
+ * Return: pointer to the new node, or NULL on failure
  */
-binary_tree_t *binary_tree_node(binary_tree_t *parent, int value) {
-    binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
-    if (new_node == NULL) {
-        return NULL;  // Failed to allocate memory
-    }
-    
-    new_node->value = value;
-    new_node->parent = parent;
-    new_node->left = NULL;
-    new_node->right = NULL;
-    
-    return new_node;
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new_node;
+
+	new_node = malloc(sizeof(binary_tree_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->parent = parent;
+	new_node->n = value;
+	new_node->left = NULL;
+	new_node->right = NULL;
+
+	return (new_node);
 }

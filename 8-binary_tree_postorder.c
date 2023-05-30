3ab7edeae0002df9e1178ce32cc393fdbbe60a80
Ayivi Angela <104152275+bloominghowl@users.noreply.github.com>
2023-05-30 15:59:12 +0000

    Update 8-binary_tree_postorder.c

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -7,12 +7,15 @@
  *
  * Description: This function traverses the binary tree in post-order
  *              and applies the given function to each node.
+ *
+ *return void
  */
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (tree == NULL || func == NULL)
 		return;
-        binary_tree_postorder(tree->left, func);
+       
+	binary_tree_postorder(tree->left, func);
 	binary_tree_postorder(tree->right, func);
 	func(tree->n);
 }

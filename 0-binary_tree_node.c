3c83e31687ce45e7f4ef974e2aafa635ccf3b7f5
Ayivi <kafuigalley7@gmail.com>
2023-05-30 07:53:51 -0700

    binary tree node

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+
+typedef struct binary_tree {
+    int value;
+    struct binary_tree *parent;
+    struct binary_tree *left;
+    struct binary_tree *right;
+} binary_tree_t;
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value) {
+    binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
+    if (new_node == NULL) {
+        return NULL;  // Failed to allocate memory
+    }
+    
+    new_node->value = value;
+    new_node->parent = parent;
+    new_node->left = NULL;
+    new_node->right = NULL;
+    
+    return new_node;
+}

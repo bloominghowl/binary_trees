5824e62e34efcefc4f03acc6aeca2d244f6f32e3
Ayivi Angela <104152275+bloominghowl@users.noreply.github.com>
2023-05-31 11:37:53 +0000

    Update 110-binary_tree_is_bst.c

--- 110-binary_tree_is_bst.c
+++ 110-binary_tree_is_bst.c
@@ -1,63 +1,70 @@
 #include "binary_trees.h"
-#include <stdbool.h>
+#include <stdio.h>
 
 /**
- * greater_than - checks if all values in the binary tree are greater than a given value
- * @tree: pointer to the root node of the binary tree
- * @val: value to compare against
+ * greater_than - check if all values in the tree are greater than a value
+ * @tree: pointer to the tree to check
+ * @val: value to check against
  *
- * Return: true if all values in the binary tree are greater than val, false otherwise
+ * Return: 1 if true, 0 if false
  */
 int greater_than(const binary_tree_t *tree, int val)
 {
+	int l, r;
+
 	if (tree == NULL)
-		return true;
-
-	if (tree->n <= val)
-		return false;
-
-	return greater_than(tree->left, val) && greater_than(tree->right, val);
+		return (1);
+	if (tree->n > val)
+	{
+		l = greater_than(tree->left, val);
+		r = greater_than(tree->right, val);
+		if (l && r)
+			return (1);
+	}
+	return (0);
 }
 
 /**
- * less_than - checks if all values in the binary tree are less than a given value
- * @tree: pointer to the root node of the binary tree
- * @val: value to compare against
+ * less_than - check if all values in the tree are less than a specific value
+ * @tree: pointer to the tree to check
+ * @val: value to check against
  *
- * Return: true if all values in the binary tree are less than val, false otherwise
+ * Return: 1 if true, 0 if false
  */
 int less_than(const binary_tree_t *tree, int val)
 {
+	int l, r;
+
 	if (tree == NULL)
-		return true;
-
-	if (tree->n >= val)
-		return false;
-
-	return less_than(tree->left, val) && less_than(tree->right, val);
+		return (1);
+	if (tree->n < val)
+	{
+		l = less_than(tree->left, val);
+		r = less_than(tree->right, val);
+		if (l && r)
+			return (1);
+	}
+	return (0);
 }
 
 /**
- * binary_tree_is_bst - checks if a binary tree is a valid Binary Search Tree (BST)
- * @tree: pointer to the root node of the binary tree
+ * binary_tree_is_bst - checks if a binary tree is a valid binary search tree
+ * @tree: pointer to the root node of the tree to check
  *
- * Return: 1 if tree is a valid BST, 0 otherwise. If tree is NULL, return 0.
+ * Return: 1 if tree is a valid BST, and 0 otherwise. If tree is NULL, return 0
  */
 int binary_tree_is_bst(const binary_tree_t *tree)
 {
 	if (tree == NULL)
-		return 0;
-
-	if (greater_than(tree->left, tree->n) && less_than(tree->right, tree->n))
+		return (0);
+	if (less_than(tree->left, tree->n) && greater_than(tree->right, tree->n))
 	{
-		if (tree->left != NULL && !binary_tree_is_bst(tree->left))
-			return 0;
+		if (!tree->left || binary_tree_is_bst(tree->left))
+		{
+			if (!tree->right || binary_tree_is_bst(tree->right))
+				return (1);
+		}
 
-		if (tree->right != NULL && !binary_tree_is_bst(tree->right))
-			return 0;
-
-		return 1;
 	}
-
-	return 0;
+	return (0);
 }

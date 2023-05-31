77dd0f52bf848809a72ba21bc8789ce2772fbef6
Ayivi Angela <104152275+bloominghowl@users.noreply.github.com>
2023-05-31 16:20:08 +0000

    Update 121-avl_insert.c

--- 121-avl_insert.c
+++ 121-avl_insert.c
@@ -1,109 +1,93 @@
 #include "binary_trees.h"
+#include <stdlib.h>
 
 /**
- * check_balance - checks the balance of each node
+ * avl_insert - Inserts a value into an AVL tree
+ * @tree: Double pointer to the root node of the AVL tree
+ * @value: Value to be inserted
  *
- * @node: pointer to the node
- * @value: input value
- * Return: no return
- */
-void check_balance(avl_t **node, int value)
-{
-	int balance;
-
-	balance = binary_tree_balance(*node);
-
-	if (balance > 1 && value < (*node)->left->n)
-	{
-		*node = binary_tree_rotate_right(*node);
-		return;
-	}
-
-	if (balance < -1 && value > (*node)->right->n)
-	{
-		*node = binary_tree_rotate_left(*node);
-		return;
-	}
-
-	if (balance > 1 && value > (*node)->left->n)
-	{
-		(*node)->left = binary_tree_rotate_left((*node)->left);
-		*node = binary_tree_rotate_right(*node);
-		return;
-	}
-
-	if (balance < -1 && value < (*node)->right->n)
-	{
-		(*node)->right = binary_tree_rotate_right((*node)->right);
-		*node = binary_tree_rotate_left(*node);
-		return;
-	}
-}
-
-/**
- * avl_in - checks if node is inserted
- *
- * @tree: tree root
- * @value: node value
- * Return: pointer to the new node
- */
-avl_t *avl_in(avl_t **tree, int value)
-{
-	avl_t *node;
-
-	if (value < (*tree)->n)
-	{
-		if ((*tree)->left == NULL)
-		{
-			(*tree)->left = binary_tree_node(*tree, value);
-			return ((*tree)->left);
-		}
-		else
-		{
-			node = avl_in(&((*tree)->left), value);
-			if (node)
-				check_balance(tree, value);
-			return (node);
-		}
-	}
-
-	if (value > (*tree)->n)
-	{
-		if ((*tree)->right == NULL)
-		{
-			(*tree)->right = binary_tree_node(*tree, value);
-			return ((*tree)->right);
-		}
-		else
-		{
-			node = avl_in(&((*tree)->right), value);
-			if (node)
-				check_balance(tree, value);
-			return (node);
-		}
-	}
-
-	return (NULL);
-}
-
-/**
- * avl_insert - inserts a value in a AVL Tree
- *
- * @tree: tree root
- * @value: node value
- * Return: pointer to the new node
+ * Return: Pointer to the newly created node, or NULL on failure
  */
 avl_t *avl_insert(avl_t **tree, int value)
 {
-	avl_t *node;
+    return avl_in(tree, value);
+}
 
-	if (*tree == NULL)
-	{
-		*tree = binary_tree_node(NULL, value);
-		return (*tree);
-	}
+/**
+ * check_balance - Checks and adjusts the balance of a node in an AVL tree
+ * @node: Double pointer to the node to check
+ * @value: Value to be inserted
+ */
+void check_balance(avl_t **node, int value)
+{
+    int balance;
 
-	node = avl_in(tree, value);
+    balance = binary_tree_balance(*node);
 
-	return (node);
+    if (balance > 1 && value < (*node)->left->n)
+    {
+        *node = binary_tree_rotate_right(*node);
+        return;
+    }
+
+    if (balance < -1 && value > (*node)->right->n)
+    {
+        *node = binary_tree_rotate_left(*node);
+        return;
+    }
+
+    if (balance > 1 && value > (*node)->left->n)
+    {
+        (*node)->left = binary_tree_rotate_left((*node)->left);
+        *node = binary_tree_rotate_right(*node);
+        return;
+    }
+
+    if (balance < -1 && value < (*node)->right->n)
+    {
+        (*node)->right = binary_tree_rotate_right((*node)->right);
+        *node = binary_tree_rotate_left(*node);
+        return;
+    }
+}
+
+/**
+ * avl_in - Recursive function to insert a value into an AVL tree
+ * @tree: Double pointer to the root node of the AVL tree
+ * @value: Value to be inserted
+ *
+ * Return: Pointer to the newly created node, or NULL on failure
+ */
+avl_t *avl_in(avl_t **tree, int value)
+{
+    avl_t *new_node = NULL;
+
+    if (*tree == NULL)
+    {
+        new_node = binary_tree_node(NULL, value);
+        if (new_node == NULL)
+            return NULL;
+
+        *tree = new_node;
+        return new_node;
+    }
+
+    if (value < (*tree)->n)
+    {
+        (*tree)->left = avl_in(&(*tree)->left, value);
+        if ((*tree)->left == NULL)
+            return NULL;
+    }
+    else if (value > (*tree)->n)
+    {
+        (*tree)->right = avl_in(&(*tree)->right, value);
+        if ((*tree)->right == NULL)
+            return NULL;
+    }
+    else
+        return NULL;
+
+    check_balance(tree, value);
+
+    return *tree;
 }
